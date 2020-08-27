d22309ea8a8dd540116a301f542e8d774c14e0c9
Arkadiusz Trzepacz <arkadiusz.trzepacz@applica.ai>
2020-08-27 15:24:37 +0200

    first prototype

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "bowling"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/Bowling.scala
@@ -0,0 +1,150 @@
+import scala.util.Random
+
+object Bowling {
+
+  val nrOfFrames = 2
+
+  trait RollResult {
+    def value(): Int
+    def desc(): String
+
+    override def toString: String = desc()
+  }
+
+  case class Normal(pins: Int) extends RollResult {
+    override def value(): Int = pins
+    override def desc(): String = s"$pins"
+  }
+
+  case class Spare(pins: Int) extends RollResult {
+    override def value(): Int = pins
+    override def desc(): String = "/"
+  }
+
+  case object Strike extends RollResult {
+    override def value(): Int = 10
+    override def desc(): String = "X"
+  }
+
+  class Player(val name: String) {
+
+    val maxNameSize = 20
+
+    def roll(previousRolls: List[RollResult]): RollResult = {
+      println(s"   Player $name rolling...")
+      Thread.sleep(2000)
+
+      val previousValues = previousRolls.map(_.value()).sum
+      val rollResult = Random.between(0, 11  - previousValues)
+
+      val result = if (rollResult == 10 && previousRolls.isEmpty) {
+        Strike
+      } else {
+        val values = previousValues + rollResult
+        if (values == 10) {
+          Spare(rollResult)
+        } else {
+          Normal(rollResult)
+        }
+      }
+      println(s"   Player $name throws $result")
+      result
+    }
+
+    def getDisplayName = {
+      name match {
+        case n if n.length < maxNameSize =>
+          val emptySpaces = " " * (maxNameSize - n.length)
+          name + emptySpaces
+        case _ => name.substring(0, maxNameSize + 1)
+      }
+    }
+  }
+
+  class Frame(val nr: Int, rolls: Map[Player, List[RollResult]]) {
+
+    def getPoints(player: Player, nextFrame: Option[Frame]): Points =  {
+      val playerRolls = getPlayerRollResults(player)
+      val pointsDesc = playerRolls.map(_.desc()).mkString("|")
+
+      val allPointsFromFrame = playerRolls.map(_.value()).sum
+
+      val allPoints: Int = playerRolls match {
+        case rolls if rolls.contains(Strike) => nextFrame.map(_.getPoints(player, None).value)
+            .getOrElse(0) + allPointsFromFrame
+        case rolls if rolls.contains(Spare) => nextFrame.map(_.getFirstRollResultValue(player))
+            .getOrElse(0) + allPointsFromFrame
+        case _ => allPointsFromFrame
+      }
+
+      new Points(nr, allPoints, pointsDesc)
+    }
+
+    private def getFirstRollResultValue(player: Player): Int = {
+      val playerRolls = getPlayerRollResults(player)
+      playerRolls.head.value()
+    }
+
+    private def getPlayerRollResults(player: Player): List[RollResult] = {
+      rolls.getOrElse(player, List())
+    }
+  }
+
+  class Points(nrOfFrame: Int, val value: Int, desc: String) {
+    def desc(): String = {
+      s"$desc ($value)"
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val players = List(new Player("Sylwia"), new Player("Arek"))
+
+    val result = play(nrOfFrames, players)
+
+    println(result)
+
+  }
+
+  private def play(nrOfFrames: Int, players: List[Player]): String = {
+    println("Bowling game is starting")
+    val frameNrs = List.range(1, nrOfFrames + 1)
+
+    val frames = frameNrs.map(nrOfFrame => {
+      println(s"Lets start frame $nrOfFrame")
+      playFrame(nrOfFrame, players)
+    })
+
+    getResultTable(players, frames)
+
+  }
+
+  private def playFrame(nrOfFrame: Int, players: List[Player]): Frame = {
+    val playerResults = players.map( player => {
+      val resultFirstRoll = player.roll(List())
+      resultFirstRoll match {
+        case Strike =>  (player, List(resultFirstRoll))
+        case _ =>
+          val resultSecondRoll = player.roll(List(resultFirstRoll))
+          (player, List(resultFirstRoll, resultSecondRoll))
+      }
+    }).toMap
+    new Frame(nrOfFrame, playerResults)
+  }
+
+  private def getResultTable(players: List[Player], frames: List[Frame]): String = {
+    println("-----RESULTS------")
+
+    players.map(player => {
+        player.getDisplayName() + ": " +
+          frames.map(frame => {
+            frame.getPoints(player, nextFrame(frame, frames)).desc()
+          }).mkString(" || ")
+    }).mkString("\n")
+
+  }
+
+  private def nextFrame(frame: Frame, allFrames: List[Frame]): Option[Frame] = {
+    allFrames.find(f => f.nr == frame.nr + 1)
+  }
+
+}
